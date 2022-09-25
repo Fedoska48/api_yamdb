58da4c2edaef570acc996c46194abaa01f1ce8da
Никита Федяев <itsfedos@gmail.com>
2022-09-25 14:38:41 +0300

    update

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,10 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-=======
     path('api/', include('api.urls')),
->>>>>>> feature/users
     path('api/', include('users.urls')),
     path(
         'redoc/',
