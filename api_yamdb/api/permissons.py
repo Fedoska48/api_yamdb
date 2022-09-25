3e241c1969080f1bd86b2b823afcb32689b95062
Никита Федяев <itsfedos@gmail.com>
2022-09-25 14:53:28 +0300

    update

---
+++ api_yamdb/api/permissons.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.user.is_staff
+                or request.method in permissions.SAFE_METHODS
+                )
